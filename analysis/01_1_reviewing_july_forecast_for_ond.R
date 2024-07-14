0acbee50df7c0344fec4d1024aa1dd65702db152
Pauline Wairimu <paunimo@gmail.com>
2024-07-15 01:06:01 +0300

    fixing the code to account for monthly values
    it looks like we would have activated the framework if following WFP methodology and not if using an overall RP.

--- analysis/01_1_reviewing_july_forecast_for_ond.R
+++ analysis/01_1_reviewing_july_forecast_for_ond.R
@@ -30,7 +30,8 @@ eth_adm1_codab <- st_read(
 trigger_proposal <- read_excel(
   file.path(
     Sys.getenv("AA_DATA_DIR"),
-    "public", "exploration", "eth", "ecmwf", "trigger_proposal.xlsx"), sheet = "Latest")
+    "public", "exploration", "eth", "ecmwf", "trigger_proposal.xlsx"), 
+  sheet = "Latest")
 
 # these are not decided on and change a lot but this is the list I came up with 
 # to be defined later
@@ -70,36 +71,54 @@ ond_forecast_lower20 <- adm3_forecast |>
   group_by(adm3_pcode) |>
   summarise(woreda_lower20 = quantile(seasonal_total, 0.2, na.rm = T))
 
+# wfp trigger mechanism: 1-in-5 year RP for every month
+ond_forecast_lower20_monthly <- adm3_forecast |>
+  dplyr::filter(pub_month == 7 & valid_month %in% c(10, 11, 12)) |>
+  mutate(year = year(valid_date)) |>
+  group_by(adm3_pcode, year, valid_month) |>
+  summarise(monthly_total = sum(value, na.rm = T)) |>
+  group_by(adm3_pcode, valid_month) |>
+  summarise(woreda_lower20 = quantile(monthly_total, 0.2, na.rm = T)) |>
+  mutate(month = month.name[valid_month])
+
 # aggregate the july forecast for Ethiopia at admin 3
 july_forecast_crop <- crop(july_forecast, raster::extent(eth_adm3_codab))
 july_forecast_adm3 <- exact_extract(july_forecast_crop, eth_adm3_codab, 
                                     "median", append_cols = c("admin3Pcode", "admin2Pcode"))
 
 
+### checking if the trigger is below the threshold for each month
+
 # append these results to one object and get the overall season performance
 july_comparison <- july_forecast_adm3 |>
-  rowwise() |>
-  mutate(season_total = sum(`median.2024-07-01.3`, 
-                            `median.2024-07-01.4`, 
-                            `median.2024-07-01.5`, na.rm = T)) |>
-  merge(ond_forecast_lower20, by.x = "admin3Pcode", by.y = "adm3_pcode") |>
+  rename_with(~ paste0(month.name[c(7:12, 1)]),
+              starts_with("median")) |>
+  pivot_longer(cols = month.name[c(7:12, 1)], 
+               names_to = "month",
+               values_to = "monthly_forecast") |> 
+  filter(month %in% month.name[c(10:12)]) |>
+  merge(ond_forecast_lower20_monthly, by.x = c("admin3Pcode", "month"), 
+        by.y = c("adm3_pcode", "month")) |>
   # Check if value if below 20th percentile
   mutate(ond_inseason = if_else(admin2Pcode %in% ond_zones, T, F), 
-         ond_woredas = if_else(admin2Pcode %in% ond_zones, woreda_lower20, NA),
-         ond_trigger = case_when(season_total > ond_woredas ~ "Above",
-                                 season_total <= ond_woredas ~ "Below",
+         ond_check = if_else(ond_inseason, woreda_lower20, NA),
+         ond_below20 = case_when(monthly_forecast > ond_check ~ "Above",
+                                 monthly_forecast <= ond_check ~ "Below",
                                  .default = NA))
 
 
 # would the trigger have activated with the triggers
 trigger_check <- july_comparison |>
-  merge(eth_adm3_codab, all.y = T) |>
+  group_by(admin3Pcode) |>
+  summarise(season_total = sum(monthly_forecast, na.rm = T)) |>
+  merge(eth_adm3_codab, by = "admin3Pcode", all.y = T) |>
   merge(trigger_proposal, by.x = "admin3Name_en", by.y = "Woreda", all.x = T) |>
   rename(july_trigger = `...8`) |>
-  mutate(trigger_check = case_when(
-    ond_inseason & season_total > july_trigger ~ "Not Reached",
-    ond_inseason & season_total <= july_trigger ~ "Reached",
-    .default = NA))
+  mutate(ond_inseason = if_else(admin2Pcode %in% ond_zones, T, F),
+         trigger_check = case_when(
+           ond_inseason & season_total > july_trigger ~ "Not Reached",
+           ond_inseason & season_total <= july_trigger ~ "Reached",
+           .default = NA))
 
 trigger_check |>
   ggplot() +
@@ -114,22 +133,83 @@ trigger_check |>
 trigger_test <- trigger_check |>
   group_by(trigger_check) |>
   summarise(count = n()) |>
-  mutate(percentage = count / sum(count) * 100) |>
-  na.omit()
+  na.omit() |>
+  mutate(percentage = count / sum(count) * 100)
 
 trigger_status <- trigger_test$percentage[trigger_test$trigger_check == "Reached"]
 
-print(paste0("The trigger would not have been reached since ", 
+print(paste0("The trigger would not have been reached since the forecast indicates ", 
              round(trigger_status, 2), 
-             "% of the OND receiving area would have had more rainfall than the July trigger."))
+             "% of the OND receiving area will have rainfall below or equal to the trigger."))
 
 # testing if ensemble mean would be below the 20%
-trigger_check |>
+july_comparison |>
+  merge(eth_adm3_codab, by = "admin3Pcode", all.y = T) |>
   ggplot() +
-  geom_sf(aes(fill = ond_trigger, geometry = Shape)) + 
+  geom_sf(aes(fill = ond_below20, geometry = Shape)) + 
+  geom_sf(data = eth_adm1_codab, fill = NA, linewidth = 0.8, color = "black") +
+  scale_fill_manual(values = c("Above" = "steelblue", "Below" = "tomato"), 
+                    na.value = "lightgrey") +
+  labs(title = "July Forecast for OND",
+       subtitle = "July forecast ensemble mean versus 20th percentile of the historical ensemble mean",
+       fill = "") +
+  facet_wrap(vars(month))
+
+ond_below20_test <- july_comparison |>
+  group_by(ond_below20, month) |>
+  summarise(count = n()) |>
+  na.omit() |>
+  group_by(month) |>
+  mutate(percentage = count / sum(count) * 100)
+
+ond_below20_test |>
+  rowwise() |>
+  filter(ond_below20 == "Below") |>
+  mutate(trigger_status = paste("The forecast indicates that for", month,
+                                 round(percentage, 2),
+                                 "% of the OND receiving area will have rainfall below or equal to the historic 20%.")) |>
+  pull(trigger_status) |>
+  walk(print)
+
+## getting the overall for the season
+seasonal_below20 <- july_comparison |>
+  group_by(admin3Pcode, admin2Pcode) |>
+  summarise(season_total = sum(monthly_forecast, na.rm = T)) |>
+  merge(ond_forecast_lower20, by.x = "admin3Pcode", by.y = "adm3_pcode") |>
+  mutate(ond_inseason = if_else(admin2Pcode %in% ond_zones, T, F),
+         below20_check = case_when(
+           ond_inseason & season_total > woreda_lower20 ~ "Above",
+           ond_inseason & season_total <= woreda_lower20 ~ "Below",
+           .default = NA))
+
+seasonal_below20 |>
+  merge(eth_adm3_codab, by = "admin3Pcode", all.y = T) |>
+  ggplot() +
+  geom_sf(aes(fill = below20_check, geometry = Shape)) + 
   geom_sf(data = eth_adm1_codab, fill = NA, linewidth = 0.8, color = "black") +
   scale_fill_manual(values = c("Above" = "steelblue", "Below" = "tomato"), 
                     na.value = "lightgrey") +
   labs(title = "July Forecast for OND",
        subtitle = "July forecast ensemble mean versus 20th percentile of the historical ensemble mean",
        fill = "")
+
+ond_below20_test_season <- seasonal_below20 |>
+  group_by(below20_check) |>
+  summarise(count = n()) |>
+  na.omit() |>
+  mutate(percentage = count / sum(count) * 100)
+
+ond_below20_test_season |>
+  rowwise() |>
+  filter(below20_check == "Below") |>
+  mutate(trigger_status = paste("The forecast indicates that",
+                                round(percentage, 2),
+                                "% of the OND receiving area will have rainfall below or equal to the historic 20%.")) |>
+  pull(trigger_status) |>
+  walk(print)
+
+## It looks like the trigger, if set using the 20% percentile for forecasts released,
+## in July would have activated. 
+## If setting an overall frequency of activation of 20% across lead times,
+## Then the RP would be less frequent. 
+## This is looking at 20% at one lead time and would activate if following WFP methodology
