344454d247ff057761351461b22077dd2f63d541
Pauline Wairimu <paunimo@gmail.com>
2024-07-22 19:56:17 +0300

    updating to have cod ab read from blob

--- analysis/01_3_wfp_woredas_triggered.R
+++ analysis/01_3_wfp_woredas_triggered.R
@@ -9,23 +9,20 @@ library(gghdx)
 gghdx()
 
 # reading in admin boundaries
-eth_adm3_codab <- st_read(
-  file.path(
-    Sys.getenv("AA_DATA_DIR"),
-    "public", "raw", "eth", "cod_ab", "Admin_2024.gdb.zip"), 
-  layer = "eth_admbnda_adm3_csa_bofedb_2024")
-
-eth_adm1_codab <- st_read(
-  file.path(
-    Sys.getenv("AA_DATA_DIR"),
-    "public", "raw", "eth", "cod_ab", "Admin_2024.gdb.zip"), 
-  layer = "eth_admbnda_adm1_csa_bofedb_2024")
+blob_endpoint <- storage_endpoint(Sys.getenv("AZURE_BLOB_ENDPOINT"), 
+                                  key = Sys.getenv("AZURE_BLOB_KEY1"))
+container <- storage_container(blob_endpoint, "projects")
+cod_ab_file <- tempfile(fileext = ".gpkg")
+eth_codab_blob <- storage_download(container, 
+                                    src = "ds-aa-eth-drought/raw/cod_ab/Admin_2024.gpkg", 
+                                    dest = cod_ab_file)
+eth_adm3_codab <- st_read(cod_ab_file, 
+                          layer = "eth_admbnda_adm3_csa_bofedb_2024")
+eth_adm1_codab <- st_read(cod_ab_file, 
+                          layer = "eth_admbnda_adm1_csa_bofedb_2024")
 
 # reading in files
 # i am sure there are better ways of downloading multiple files
-blob_endpoint <- storage_endpoint("https://imb0chd0dev.blob.core.windows.net", 
-                                  key = Sys.getenv("AZURE_BLOB_KEY1"))
-container <- storage_container(blob_endpoint, "projects")
 som_temp_file <- tempfile(fileext = ".csv")
 somali_csv_blob <- storage_download(container, 
                                     src = "ds-aa-eth-drought/exploration/AA Dashboard - Somali Region, OND Season.csv", 
@@ -47,7 +44,7 @@ ond_woredas_triggered |>
   filter(Severity == "Moderate") |>
   merge(eth_adm3_codab, by.x = "Woreda", by.y = "admin3Name_en", all.y = T) |>
   ggplot() +
-  geom_sf(aes(fill = `Triggered?`, geometry = Shape)) + 
+  geom_sf(aes(fill = `Triggered?`, geometry = geom)) + 
   geom_sf(data = eth_adm1_codab, fill = NA, linewidth = 0.8, color = "black") +
   scale_fill_manual(values = c("Yes" = "tomato"), na.translate = FALSE) +
   labs(title = "Moderate Drought Trigger Status for OND 2024 in Ethiopia",
@@ -58,7 +55,7 @@ ond_woredas_triggered |>
   filter(Severity == "Severe") |>
   merge(eth_adm3_codab, by.x = "Woreda", by.y = "admin3Name_en", all.y = T) |>
   ggplot() +
-  geom_sf(aes(fill = `Triggered?`, geometry = Shape)) + 
+  geom_sf(aes(fill = `Triggered?`, geometry = geom)) + 
   geom_sf(data = eth_adm1_codab, fill = NA, linewidth = 0.8, color = "black") +
   scale_fill_manual(values = c("Yes" = "tomato"), na.translate = FALSE) +
   labs(title = "Severe Drought Trigger Status for OND 2024 in Ethiopia",
@@ -72,7 +69,7 @@ ond_woredas_triggered |>
   merge(eth_adm3_codab, by.x = "Woreda", by.y = "admin3Name_en", all.y = TRUE) |>
   mutate(Severity = ifelse(is.na(Severity), NA, Severity)) |>
   ggplot() + 
-  geom_sf(aes(fill = Severity, geometry = Shape)) + 
+  geom_sf(aes(fill = Severity, geometry = geom)) + 
   geom_sf(data = eth_adm1_codab, fill = NA, linewidth = 0.8, color = "black") +
   scale_fill_manual(values = c("Severe" = "tomato", "Moderate" = "orange"),
                     labels = c("Moderate (1-in-3 year event)", "Severe (1-in-5 year event)"), na.value = "lightgrey", na.translate = FALSE) +
